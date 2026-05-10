027cb3b8a66e9e807ca67e77e4ede79989fcf5f2
Firas Mosbehi <firas.mosbehi@insat.ucar.tn>
2026-05-10 18:25:34 +0200

    fix: correct .gitignore to not ignore cmd/envguard directory

---
+++ cmd/envguard/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"os"
+
+	"github.com/envguard/envguard/internal/cli"
+)
+
+const version = "0.1.0"
+
+func main() {
+	if err := cli.Execute(version); err != nil {
+		if errors.Is(err, cli.ErrValidationFailed) {
+			os.Exit(1)
+		}
+		if errors.Is(err, cli.ErrIO) {
+			os.Exit(2)
+		}
+		os.Exit(1)
+	}
+}
